ae3950c509be7fc82770377da3528ee6109ae2df
ogoiddev <diogompantaleao@gmail.com>
2022-10-07 13:00:08 -0300

    deploy: variables defined

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'VL8OUyFnqVTdNXMzwSd4',
-  database: 'TRYBE_FUTEBOL_CLUBE' || 'railway',
-  host: process.env.DB_HOST || 'containers-us-west-80.railway.app',
-  port: Number(process.env.DB_PORT) || 5852,
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'VL8OUyFnqVTdNXMzwSd4',
+  database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
+  host: process.env.MYSQLHOST || 'containers-us-west-80.railway.app',
+  port: Number(process.env.MYSQLPORT) || 5852,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
