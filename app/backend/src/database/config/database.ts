fb091c6056064b068c6a67a9da5828c33503d179
ogoiddev <diogompantaleao@gmail.com>
2022-10-07 11:56:18 -0300

    deploy: railway try

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,10 +3,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  password: process.env.DB_PASS || 'VL8OUyFnqVTdNXMzwSd4',
+  database: 'TRYBE_FUTEBOL_CLUBE' || 'railway',
+  host: process.env.DB_HOST || 'containers-us-west-80.railway.app',
+  port: Number(process.env.DB_PORT) || 5852,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
